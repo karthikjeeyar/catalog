7164f8d4d6aa11db1f08ee36976f87e71708a7d8
karthik <karthik.jk11@gmail.com>
2022-04-08 15:53:25 +0530

    Add my custom catalog

--- task/upload-pypi/0.2/tests/pre-apply-task-hook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add pypiserver sidecar
-add_sidecars ${TMPF} '{"image":"pypiserver/pypiserver:latest", "name": "pypiserver", "args": ["-P .", "-a ."]}'
-
-# Add git-clone
-add_task git-clone latest
